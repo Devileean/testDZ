e0853aadb86e996877340f05424b9897954844a3
AlexseyLoginov <devileean@gmail.com>
2022-12-22 02:32:48 +0300

    add method get strings

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формируст массив из строк, длина которых
+// меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте
+// выполнения алгоритма. При решении но рекомендуется пользоваться коллекциями, лучше обойтись
+// исключительно массивами.
+
+
+var gettext = GetAnyArray("Введите элементы массива через пробел");
+Console.WriteLine(String.Join(",", gettext));
+
+string[] GetAnyArray(string outputText = "", bool inline = false)
+{
+    var arrays = Array.Empty<string>();
+    if (inline)
+        Console.Write(outputText);
+    else
+        Console.WriteLine(outputText);
+    try
+    {
+        char[] separators = { ' ', ',' };
+        var arrayOfEnteredText = Console.ReadLine()
+            ?.Split(separators,
+                StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+        if (arrayOfEnteredText != null)
+            arrays = Array.ConvertAll(arrayOfEnteredText, s => s);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        throw;
+    }
+
+    return arrays;
+}
